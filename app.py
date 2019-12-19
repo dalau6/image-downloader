cd115870f69b4dd466a921c8e58c46b85a74d9cd
David <dalau6@gmail.com>
2019-12-18 23:06:52 -0800

    added handling for missing image link.

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-import requests
+import io
+from PIL import Image
 
-res = requests.get(img_url, stream=True)
-count = 1
-while res.status_code != 200 and count <= 5:
+def image_downloader(img_url: str):
+    """
+    Input:
+    param: img_url  str (Image url)
+    Tries to download the image url and use name provided in headers. Else it randomly picks a name
+    """
+    print(f'Downloading: {img_url}')
     res = requests.get(img_url, stream=True)
-    print(f'Retry: {count} {img_url}')
-    count += 1
+    count = 1
+    while res.status_code != 200 and count <= 5:
+        res = requests.get(img_url, stream=True)
+        print(f'Retry: {count} {img_url}')
+        count += 1
+    # checking the type for image
+    if 'image' not in res.headers.get("content-type", ''):
+        print('ERROR: URL doesnot appear to be an image')
+        return False
+    # Trying to red image name from response headers
+    try:
+        image_name = str(img_url[(img_url.rfind('/')) + 1:])
+        if '?' in image_name:
+            image_name = image_name[:image_name.find('?')]
+    except:
+        image_name = str(random.randint(11111, 99999))+'.jpg'
+
+    i = Image.open(io.BytesIO(res.content))
+    download_location = get_download_location()
+    i.save(download_location + '/'+image_name)
+    return f'Download complete: {img_url}'
